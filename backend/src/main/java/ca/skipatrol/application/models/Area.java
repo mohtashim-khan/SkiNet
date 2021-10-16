ce2df1e52dd0a42655a9d2f85960d5d734f6fc14
Savi Jessel <savipal.jessel@gmail.com>
2021-10-15 19:36:56 -0600

    Added Area model and repo

---
+++ backend/src/main/java/ca/skipatrol/application/models/Area.java
@@ -0,0 +1,28 @@
+package ca.skipatrol.application.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.EnumType.STRING;
+
+@Entity
+public class Area {
+
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    @Column(unique = true, nullable = false)
+    private String areaname;
+
+    private Area() {
+    }
+
+    public Area(String areaname) {
+        this.areaname = areaname;
+
+    }
+
+}

68004dcb8b61573632775374299e2f55a7f4db88
BNWong2000 <brandenw2000@gmail.com>
2021-11-14 22:47:19 -0700

    Added Action Log repo and Model

---
+++ backend/src/main/java/ca/skipatrol/application/repositories/Action_LogRepository.java
@@ -0,0 +1,12 @@
+package ca.skipatrol.application.repositories;
+import ca.skipatrol.application.models.Action_Log;
+import ca.skipatrol.application.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface Action_LogRepository extends JpaRepository<Action_LogRepository,Long> {
+    Optional<Action_Log> findByEvent(Event event);
+
+    Action_Log save(Action_Log action_log);
+}

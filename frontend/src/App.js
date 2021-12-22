4ff8f8eb4041d9bf383dc739138a855c8b45fc42
Salah Sheikh <salah.h.sheikh@gmail.com>
2021-12-21 17:34:17 -0700

    Improve 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import React, {Component, useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component, useState, useEffect } from "react";
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-
 
 import {
   ProtectedRoute,
@@ -31,11 +30,27 @@ import PersonnelPage from "./pages/Personnel/Personnel";
 
 const NotFound = () => {
   return (
-      <>
-          <p>Page not found!</p>
-      </>
-  )
-}
+    <>
+      <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">Page not found!</h1>
+            <p class="lead text-muted">
+              The page you are looking for does not exist. How you got here is a
+              mystery. But you can click the button below to go back to the
+              homepage.
+            </p>
+            <p>
+              <Link to="/" class="btn btn-primary my-2">
+                Home
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 class App extends Component {
   constructor(props) {
@@ -44,49 +59,49 @@ class App extends Component {
   }
 
   render = () => {
-    return (<Router>
-      <Header
-        session={this.session}
-      />
-      <Switch>
-        <ProtectedRoute
-          path="/roster/:event_id"
-          Component={Roster}
-          session={this.session}
-        />
-        <ProtectedRoute
-          path="/user"
-          Component={UserPage}
-          session={this.session}
-        />
+    return (
+      <Router>
+        <Header session={this.session} />
+        <Switch>
+          <ProtectedRoute
+            path="/roster/:event_id"
+            Component={Roster}
+            session={this.session}
+          />
+          <ProtectedRoute
+            path="/user"
+            Component={UserPage}
+            session={this.session}
+          />
 
-        <ProtectedRoute
-          path="/users/:usernameParam"
-          Component={OtherUserPage}
-          session={this.session}
-        />
+          <ProtectedRoute
+            path="/users/:usernameParam"
+            Component={OtherUserPage}
+            session={this.session}
+          />
 
-        <ProtectedRoute
-          path="/personnel"
-          Component={PersonnelPage}
-          session={this.session}
-        />
+          <ProtectedRoute
+            path="/personnel"
+            Component={PersonnelPage}
+            session={this.session}
+          />
 
-        {/* Can access if they Are NOT Signed in */}
-        <ProtectedLogin
-          path="/sign-in"
-          Component={SignIn}
-          session={this.session}
-        />
+          {/* Can access if they Are NOT Signed in */}
+          <ProtectedLogin
+            path="/sign-in"
+            Component={SignIn}
+            session={this.session}
+          />
 
-        {/* Unprotected Can Access by Anyone */}
-        <Route path="/sign-up" />
-        <Route exact path="/test" component={Roster} />
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-      <Footer />
-    </Router>)
+          {/* Unprotected Can Access by Anyone */}
+          <Route path="/sign-up" />
+          <Route exact path="/test" component={Roster} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </Router>
+    );
   };
 }
 
@@ -95,7 +110,7 @@ function App2() {
     CookieService.get("type") ? "Successful" : "Not Attempted"
   );
   const [userAuth, setAuth] = useState(
-      CookieService.get("userDetails") !== undefined
+    CookieService.get("userDetails") !== undefined
       ? CookieService.get("userDetails")
       : {
           username: "",

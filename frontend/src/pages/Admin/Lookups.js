b9f24bc727688d873a01470ea19d85251488341a
Salah Sheikh <salah.h.sheikh@gmail.com>
2021-12-27 12:24:34 -0700

    Refactor lookups page to use new react component, avoid memleak

--- frontend/src/pages/Admin/Lookups.js
+++ frontend/src/pages/Admin/Lookups.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import './Lookups.css'
+import "./Lookups.css";
 
 const SeasonsLookupComponent = ({ session }) => {
   const [season, setSeasons] = useState([]);
@@ -11,34 +11,51 @@ const SeasonsLookupComponent = ({ session }) => {
         setSeasons(resp.data._embedded.seasons);
       }
     });
-  });
+  }, [season]);
 
   return (
     <div class="col-4 p-3">
-    <h5>Seasons</h5>
-    <div class="overflow-auto" data-spy="scroll">
-      <ul class="list-group scrollableList ">
-        {season.map((row) => (
-          <li onClick={() => {
-            if (selectedSeason.has(row.description)) {
-              selectedSeason.delete(row.description);
-            } else {
-              selectedSeason.add(row.description);
-            }
-            setSelectedSeasons(selectedSeason);
-          }} className={"list-group-item " + (selectedSeason.has(row.description) ? "active" : "")}>{row.description}</li>
-        ))}
-      </ul>
+      <h5>Seasons</h5>
+      <div class="overflow-auto" data-spy="scroll">
+        <ul class="list-group scrollableList ">
+          {season.map((row) => (
+            <li
+              key={row.description}
+              onClick={() => {
+                if (selectedSeason.has(row.description)) {
+                  selectedSeason.delete(row.description);
+                } else {
+                  selectedSeason.add(row.description);
+                }
+                setSelectedSeasons(selectedSeason);
+              }}
+              className={"list-group-item " + (selectedSeason.has(row.description) ? "active" : "")}
+            >
+              {row.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="d-flex flex-row-reverse mt-1">
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-secondary">
+            Left
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Middle
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Right
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
   );
-}
+};
 
-const AdminLookupsPage = ({ session }) => {
-
+const JacketBrandLookup = ({ session }) => {
   const [brand, setBrands] = useState([]);
-  const [discipline, setDisciplines] = useState([]);
-  const [award, setAwards] = useState([]);
+  const [selectedBrand, setSelectedBrands] = useState(new Set());
 
   useEffect(() => {
     session.get("brands").then((resp) => {
@@ -46,74 +63,59 @@ const AdminLookupsPage = ({ session }) => {
         setBrands(resp.data._embedded.brands);
       }
     });
+  }, [brand]);
 
-    session.get("disciplines").then((resp) => {
-      if (resp.status === 200) {
-        setDisciplines(resp.data._embedded.disciplines);
-      }
-    });
+  return (
+    <div class="col-4 p-3">
+      <h5>Jacket Brand</h5>
+      <div class="overflow-auto" data-spy="scroll">
+        <ul class="list-group scrollableList ">
+          {brand.map((row) => (
+            <li
+              // TODO: Change key to row.id when id becomes visible from the API
+              key={row.description}
+              onClick={() => {
+                if (selectedBrand.has(row.description)) {
+                  selectedBrand.delete(row.description);
+                } else {
+                  selectedBrand.add(row.description);
+                }
+                setSelectedBrands(selectedBrand);
+              }}
+              className={"list-group-item " + (selectedBrand.has(row.description) ? "active" : "")}
+            >
+              {row.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div class="d-flex flex-row-reverse mt-1">
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-secondary">
+            Left
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Middle
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Right
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    session.get("awards").then((resp) => {
-      if (resp.status === 200) {
-        setAwards(resp.data._embedded.awards);
-      }
-    });
-  });
-
+const AdminLookupsPage = ({ session }) => {
   return (
     <>
       <div class="container">
         <div class="row">
-          <div class="col-4 p-3">
-            <h5>External - Instruction/Coaching</h5>
-            <div class="overflow-auto" data-spy="scroll">
-              <div class="list-group scrollableList ">
-                <a href="#" class="list-group-item list-group-item-action"
-                >CSIA - Level I </a>
-                <a href="#" class="list-group-item list-group-item-action">CSIA - Level II</a>
-                <a href="#" class="list-group-item list-group-item-action">CSIA - Level III</a>
-                <a href="#" class="list-group-item list-group-item-action">CSIA - Level IV</a>
-                <a href="#" class="list-group-item list-group-item-action">CASI - Level 1</a>
-                <a href="#" class="list-group-item list-group-item-action">CASI - Level 1</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-4 p-3">
-            <h5>Jacket Brand</h5>
-            <div class="overflow-auto" data-spy="scroll">
-              <ul class="list-group scrollableList ">
-                {brand.map((row) => (
-                  <li class="list-group-item">{row.description}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div class="col-4 p-3">
-            <h5>Lake Louise Awards</h5>
-            <div class="overflow-auto" data-spy="scroll">
-              <ul class="list-group scrollableList ">
-                {award.map((row) => (
-                  <li class="list-group-item">{row.description}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
+          <JacketBrandLookup session={session} />
         </div>
 
         <div class="row">
-          <div class="col-4 p-3">
-            <h5>On-Snow Evaluation Disciplines</h5>
-            <div class="overflow-auto" data-spy="scroll">
-              <ul class="list-group scrollableList ">
-                {discipline.map((row) => (
-                  <li class="list-group-item">{row.description}</li>
-                ))}
-              </ul>
-              <button>testButton</button>
-            </div>
-          </div>
           <SeasonsLookupComponent session={session} />
-          <div class="col-4 p-3">.col-4</div>
         </div>
       </div>
     </>

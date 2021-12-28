822bf8b2f119a4d61a6fd888ee54b945471b738f
Salah Sheikh <salah.h.sheikh@gmail.com>
2021-12-28 13:24:22 -0700

    Expose ids for look-up models

---
+++ backend/src/main/java/ca/skipatrol/application/VisibleFieldConfiguration.java
@@ -0,0 +1,22 @@
+package ca.skipatrol.application;
+
+import ca.skipatrol.application.models.Award;
+import ca.skipatrol.application.models.Brand;
+import ca.skipatrol.application.models.Season;
+import ca.skipatrol.application.models.Size;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class VisibleFieldConfiguration implements RepositoryRestConfigurer {
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
+        config.exposeIdsFor(Brand.class);
+        config.exposeIdsFor(Award.class);
+        config.exposeIdsFor(Season.class);
+        config.exposeIdsFor(Size.class);
+    }
+
+}

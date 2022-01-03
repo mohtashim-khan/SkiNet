8b56d8b70efdd528e42a57ae4bf0a04e2bc04ca1
Dan Tran <dgtran01@gmail.com>
2022-01-03 13:26:06 -0700

    Refactor LookupController logic to LookupServices

--- backend/src/main/java/ca/skipatrol/application/services/LookupServicesImpl.java
+++ backend/src/main/java/ca/skipatrol/application/services/LookupServicesImpl.java
@@ -2,26 +2,41 @@ package ca.skipatrol.application.services;
 
 import ca.skipatrol.application.Interfaces.LookupServices;
 import ca.skipatrol.application.models.Season;
-import ca.skipatrol.application.repositories.SeasonRepository;
-import ca.skipatrol.application.repositories.UserRepository;
+import ca.skipatrol.application.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
 public class LookupServicesImpl implements LookupServices {
 
+    //region Lookup Repository Declarations
+    @Autowired
+    OperationalEventRepository operationalEventRepository;
+    @Autowired
+    DisciplineRepository disciplineRepository;
+    @Autowired
+    BrandRepository brandRepository;
+    @Autowired
+    SizeRepository sizeRepository;
+    @Autowired
+    ConditionsRepository conditionsRepository;
     @Autowired
     SeasonRepository seasonRepository;
+    @Autowired
+    AwardRepository awardRepository;
+    //endregion
+
+
 
     public void saveSeason(Season season) {
         List<Season> seasonList = seasonRepository.findAll();
         season.setSequence(seasonList.size() + 1);
         seasonRepository.save(season);
-
     }
 
     public void deleteSeason(long id) {
@@ -34,9 +49,72 @@ public class LookupServicesImpl implements LookupServices {
                 Season updatedSeason = seasonList.get(i);
                 updatedSeason.setSequence(updatedSeason.getSequence() - 1);
                 seasonRepository.save(updatedSeason);
-
             }
         }
-
     }
+
+    public boolean deleteBrandsInBatch(ArrayList<Long> ids) {
+        try
+        {
+            brandRepository.deleteAllByIdInBatch(ids);
+            for (Long id : ids) {
+                assert (brandRepository.findById(id).isEmpty());
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public boolean deleteAwardsInBatch(ArrayList<Long> ids)
+    {
+        try
+        {
+            awardRepository.deleteAllByIdInBatch(ids);
+            for (Long id : ids) {
+                assert (awardRepository.findById(id).isEmpty());
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public boolean deleteSeasonsInBatch(ArrayList<Long> ids)
+    {
+        try
+        {
+            for (Long id : ids) {
+                this.deleteSeason(id);
+                assert (seasonRepository.findById(id).isEmpty());
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public boolean deleteDisciplineInBatch(ArrayList<Long> ids)
+    {
+        try
+        {
+            disciplineRepository.deleteAllByIdInBatch(ids);
+            for (Long id : ids) {
+                assert (disciplineRepository.findById(id).isEmpty());
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+
 }

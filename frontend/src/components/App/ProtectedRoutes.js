b804e622febf1cff2365065d9cd72dd20b09f6b0
Salah Sheikh <salah.h.sheikh@gmail.com>
2022-01-05 17:34:44 -0700

    Prevent render of protected components unless signed in

--- frontend/src/components/App/ProtectedRoutes.js
+++ frontend/src/components/App/ProtectedRoutes.js
@@ -1,6 +1,17 @@
 import { Route, Redirect } from "react-router-dom";
 
 export const ProtectedRoute = ({ Component, session, ...rest }) => {
+  console.log("protected route" + session.logged_in());
+
+  if (session.logged_in()) {
+    return (<Route {...rest} render={() => (<Component session={session} />)} />);
+  } else {
+    return <Redirect to="/sign-in" />;
+  }
+};
+
+export const HomeRoute = ({ Component, session, ...rest }) => {
+  console.log(session.logged_in());
   return (
     <Route
       {...rest}

815423fc60fd04b1d4f4d7732c303c85bac61f86
Salah Sheikh <salah.h.sheikh@gmail.com>
2022-01-05 17:38:03 -0700

    Remove extraneous logging calls

--- frontend/src/components/App/ProtectedRoutes.js
+++ frontend/src/components/App/ProtectedRoutes.js
@@ -1,8 +1,6 @@
 import { Route, Redirect } from "react-router-dom";
 
 export const ProtectedRoute = ({ Component, session, ...rest }) => {
-  console.log("protected route" + session.logged_in());
-
   if (session.logged_in()) {
     return <Route {...rest} render={() => <Component session={session} />} />;
   } else {
@@ -11,7 +9,6 @@ export const ProtectedRoute = ({ Component, session, ...rest }) => {
 };
 
 export const HomeRoute = ({ Component, session, ...rest }) => {
-  console.log(session.logged_in());
   return (
     <Route
       {...rest}

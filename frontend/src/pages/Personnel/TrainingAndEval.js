252c639cadd38f492c60c7a5300d9333f19083c7
Savi Jessel <savipal.jessel@gmail.com>
2022-01-10 17:30:26 -0700

    Minor onSnowEval changes and extended save functionality for Training and Eval

--- frontend/src/pages/Personnel/TrainingAndEval.js
+++ frontend/src/pages/Personnel/TrainingAndEval.js
@@ -10,6 +10,7 @@ const TrainingAndEval = ({ session, userID }) => {
 
   const [onSnowEvals, setOnSnowEvals] = useState([]);
   const [operationalTraining, setOperationalTraining] = useState([]);
+  const [operationalEvent, setOperationalEvent] = useState([])
   const [evaluationTraining, setEvaluationTraining] = useState([]);
   const [editPrompted, setEditPrompted] = useState(false);
   const [user, setUser] = useState([]);
@@ -40,6 +41,26 @@ const TrainingAndEval = ({ session, userID }) => {
     console.log("fuck you");
   }
 
+  function addOnSnowEval() {
+    try {
+      const myDate = new Date($("#OnSnowTrainingDate").val()).toISOString()
+      const myDiscipline = $("#OnSnowDisciplines").val();
+      const myEval = $("#OnSnowEvalBy").val();
+      if (myEval.length === 0 || myDiscipline === -1) {
+        throw 'empty eval';
+      }
+      session.post("onSnowEvals", {
+        evaluationDate: myDate, discipline: discipline[myDiscipline]._links.self.href, evaluatedBy: myEval, user: user._links.self.href
+        // CHECK CHAT I'm getting _links not found ---- unrelated I was getting errors on the backend when i included evaluatedBy in the backend as well
+      }, {}, false).then(() => { readNewTrainingAndEvals() })
+      promptAddCancel()
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+
+    console.log("ASFASDFS", user._links.self.href)
+  }
 
   function addEvalTraining() {
 
@@ -63,6 +84,29 @@ const TrainingAndEval = ({ session, userID }) => {
 
   }
 
+  function addOperationalTraining() {
+
+    try {
+      const myDate = new Date($("#OperationalTrainingDate").val()).toISOString()
+      const myOperationalIndex = $("#OperationalTrainingEvent").val();
+
+      if (myOperationalIndex === -1) {
+        throw 'empty operational';
+      }
+
+      console.log(myOperationalIndex);
+      session.post("operationalTrainings", {
+        completedDate: myDate, operationalEvent: operationalEvent[myOperationalIndex]._links.self.href, user: user._links.self.href
+
+      }, {}, false).then(() => { readNewTrainingAndEvals() })
+      promptAddCancel()
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+
+  }
+
   const AddEval = () => {
     if (type === "1") {
       return (
@@ -70,56 +114,65 @@ const TrainingAndEval = ({ session, userID }) => {
           <Alert variant="danger" show={error} onClose={() => setError(false)} dismissible={true}>
             <Alert.Heading>Uh oh!</Alert.Heading>
             <p>
-              Looks like you entered an incorrect value for one of the fields!
+              Looks like you need glasses
             </p>
           </Alert>
-          <h5>On Snow Evaluation</h5>
+          <h5>Patroller On-Snow Evaluation</h5>
+
           <div class="input-group mb-3">
             <div class="input-group-prepend">
               <label class="input-group-text" for="inputGroupSelect01">
-                Discipline Type
+                Discipline:
               </label>
+
             </div>
 
-            <select class="custom-select" id="inputGroupSelect01">
-              <option selected>Choose...</option>
-              {discipline.map((row) => (
-                <option value={row}>{row.description}</option>
+            <select class="custom-select" id="OnSnowDisciplines">
+              <option selected value={-1}>Choose...</option>
+              {discipline.map((row, index) => (
+                <option value={index}>{row.description}</option>
               ))}
             </select>
           </div>
+
           <div class="input-group mb-3">
             <div class="input-group-prepend">
-              <label class="input-group-text">Date Evaluated</label>
+              <label class="input-group-text">Date Completed</label>
             </div>
 
             <Form.Control
               type="date"
               name="date_of_birth"
-              value={date}
-              onChange={(e) => {
-                setDate(e.target.value);
-              }}
+              id="OnSnowTrainingDate"
+            // value={date}
+            // onChange={(e) => {
+            //   setDate(e.target.value);
+            // }}
             />
           </div>
 
           <div class="input-group mb-3">
             <div class="input-group-prepend">
               <label class="input-group-text" for="inputGroupSelect01">
-                Evaluated By
+                Evaluated by:
               </label>
             </div>
             <input
               type="text"
               class="form-control"
-              id="exampleInputEmail1"
+              id="OnSnowEvalBy"
+              name="myEvalInput"
               aria-describedby="emailHelp"
-            //placeholder={date}
+
+
             />
           </div>
-          <Button variant="primary" >
+
+
+          <Button variant="primary" onClick={addOnSnowEval}>
             Submit
           </Button>
+
         </>
       );
     } else if (type === "2") {
@@ -177,24 +230,24 @@ const TrainingAndEval = ({ session, userID }) => {
           <Alert variant="danger" show={error} onClose={() => setError(false)} dismissible={true}>
             <Alert.Heading>Uh oh!</Alert.Heading>
             <p>
-              Looks like you entered an incorrect value for one of the fields!
+              Looks like you need glasses
             </p>
           </Alert>
           <h5>Patroller Operational Training</h5>
+
           <div class="input-group mb-3">
             <div class="input-group-prepend">
               <label class="input-group-text" for="inputGroupSelect01">
                 Event Type
               </label>
             </div>
-            <input
-              type="text"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder={"Event Type"}
-              onChange={(e) => { setTheEventType(e.target.value) }}
-            />
+
+            <select class="custom-select" id="OperationalTrainingEvent">
+              <option selected value={-1}>Choose...</option>
+              {operationalEvent.map((row, index) => (
+                <option value={index}>{row.description}</option>
+              ))}
+            </select>
           </div>
 
           <div class="input-group mb-3">
@@ -205,16 +258,17 @@ const TrainingAndEval = ({ session, userID }) => {
             <Form.Control
               type="date"
               name="date_of_birth"
-              value={date}
-              onChange={(e) => {
-                setDate(e.target.value);
-              }}
+              id="OperationalTrainingDate"
+            // value={date}
+            // onChange={(e) => {
+            //   setDate(e.target.value);
+            // }}
             />
-
           </div>
-          <Button variant="primary">
+          <Button variant="primary" onClick={addOperationalTraining}>
             Submit
           </Button>
+
         </>
       );
     }
@@ -233,7 +287,7 @@ const TrainingAndEval = ({ session, userID }) => {
           setOnSnowEvals(resp.data.onSnowEvals);
           setOperationalTraining(resp.data.operationalTrainings);
           setEvaluationTraining(resp.data.evalTrainings);
-          //console.log(evaluationTraining);
+          console.log(operationalTraining);
         }
       });
   }
@@ -244,6 +298,13 @@ const TrainingAndEval = ({ session, userID }) => {
         setUser(resp.data);
       }
     });
+
+    session.get("operationalEvents").then((resp) => {
+      if (resp.status === 200) {
+        setOperationalEvent(resp.data._embedded.operationalEvents);
+
+      }
+    })
 
     session.get("disciplines").then((resp) => {
       if (resp.status === 200) {

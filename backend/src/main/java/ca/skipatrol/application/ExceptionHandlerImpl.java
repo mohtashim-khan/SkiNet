0b769b4b9da1bafb29ac7712d6d20b9be1c7cf56
Salah Sheikh <salah.h.sheikh@gmail.com>
2022-02-20 19:59:36 -0700

    Return HTTP error code of 400 for all exceptions

--- backend/src/main/java/ca/skipatrol/application/ExceptionHandlerImpl.java
+++ backend/src/main/java/ca/skipatrol/application/ExceptionHandlerImpl.java
@@ -1,5 +1,6 @@
 package ca.skipatrol.application;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -11,19 +12,19 @@ import java.util.HashMap;
 public class ExceptionHandlerImpl {
 
     @ExceptionHandler(Exception.class)
-    public HashMap<String, String> handleException(HttpServletRequest request, Exception e) {
+    public ResponseEntity<HashMap<String, String>> handleException(HttpServletRequest request, Exception e) {
         HashMap<String, String> response = new HashMap<>();
         response.put("status", "error");
         response.put("message", e.getMessage());
-        return response;
+        return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public HashMap<String, String> handleNotFoundResourceException(HttpServletRequest request, NoHandlerFoundException e) {
+    public ResponseEntity<HashMap<String, String>> handleNotFoundResourceException(HttpServletRequest request, NoHandlerFoundException e) {
         HashMap<String, String> response = new HashMap<>();
         response.put("status", "error");
         response.put("message", "Requested endpoint is unavailable");
-        return response;
+        return ResponseEntity.badRequest().body(response);
     }
 
 }
